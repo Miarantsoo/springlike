e96ba623627cb3c12a8e7a8c538c04d39c0294a3
Miarantsoa Ainaharison <miarantsoaainaharison@gmail.com>
2024-05-15 14:45:29 +0300

    sprint1 done

---
+++ src/itu/etu2779/annotation/Controller.java
@@ -0,0 +1,12 @@
+package itu.etu2779.annotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.TYPE)
+public @interface Controller {
+    
+}
