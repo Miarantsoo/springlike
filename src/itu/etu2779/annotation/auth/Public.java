f9cccf317ef7335fca368e593dcc52226361862e
Miarantsoa Ainaharison <miarantsoaainaharison@gmail.com>
2025-02-18 06:33:18 +0300

    sprint16 2779

--- src/itu/etu2779/annotation/auth/Public.java
+++ src/itu/etu2779/annotation/auth/Public.java
@@ -6,8 +6,7 @@ import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 @Retention(RetentionPolicy.RUNTIME)
-@Target(ElementType.TYPE)
-public @interface Auth {
+@Target(ElementType.METHOD)
+public @interface Public {
     
-    public RoleType value() default RoleType.Default;
 }
